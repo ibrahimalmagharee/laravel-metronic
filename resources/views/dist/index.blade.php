063b4b0559925da947f58159bfa812799f3ed1a0
ibrahimalmagree <ibrahimalmagree@gmail.com>
2022-06-13 17:14:10 +0300

    first commit

---
+++ resources/views/dist/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.dashboard')
+
+@section('content')
+				<!--begin::Aside-->
+
+				<!--end::Aside-->
+				<!--begin::Wrapper-->
+					<!--begin::Header-->
+					<!--end::Header-->
+					<!--begin::Content-->
+					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+
+						<div class="post d-flex flex-column-fluid" id="kt_post">
+							<!--begin::Container-->
+							<div id="kt_content_container" class="container-xxl">
+								Dashboard
+							</div>
+							<!--end::Container-->
+						</div>
+						<!--end::Post-->
+					</div>
+
+
+
+@endsection
+
+@section('script')
+    @endsection
